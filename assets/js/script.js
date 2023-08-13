bd3edcbc5cbfa0d9810c1e6b40ed5a4d2f0c597f
Robine <Robine@macbook-robine.home>
2023-08-13 10:12:36 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM chargé");
+  // -- Détecter un click n'importe où sur la page
+  //   document.addEventListener("click", () => {
+
+  document
+    .querySelector("#contactForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      // console.log("submit", event);
+
+      const firstname = document.querySelector("#firstname").value;
+      const lastname = document.querySelector("#lastname").value;
+      const email = document.querySelector("#email").value;
+      const subject = document.querySelector("#subject").value;
+      const message = document.querySelector("#message").value;
+
+      console.log({
+        firstname,
+        lastname,
+        email,
+        subject,
+        message,
+      });
+
+      const { data } = await axios.post("http://localhost:3000/form", {
+        firstname,
+        lastname,
+        email,
+        subject,
+        message,
+      });
+
+      // console.log("response>>", response.data);
+      console.log("response =>", data);
+    });
+});
